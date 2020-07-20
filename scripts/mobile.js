e06f0b3c7e2e968aac2caf3ab4090c8cfb0e2b2d
Miriam <mlam27@calpoly.edu>
2020-07-20 02:13:09 -0700

    fun movile

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -3,8 +3,10 @@ var nav = document.getElementById('nav-list');
 
 $('#menu-icon').click(function (){
     nav.classList.toggle('hide-mobile');
+    nav.classList.toggle('cover');
 });
 
 $('#exit-icon').click(function (){
     nav.classList.add('hide-mobile');
+    nav.classList.toggle('cover');
 });
